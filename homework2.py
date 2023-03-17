4b9429ab3d5e286c3a41de6ce519f9c64dc7c71e
trongphu567 <trongphu543210@gmail.com>
2023-03-17 17:27:22 +0700

    first commit

--- homework2.py
+++ homework2.py
@@ -1,15 +1,15 @@
-x=8
-y=2
-print(x+y*3)
-print((x+y)*3)
-print(x**y)
-print(x%y)
-print(x/12.0)
-print(x//6)
+# x=8
+# y=2
+# print(x+y*3)
+# print((x+y)*3)
+# print(x**y)
+# print(x%y)
+# print(x/12.0)
+# print(x//6)
 
-x=54.66
-print(round(x))
-print(int(x))
+# x=54.66
+# print(round(x))
+# print(int(x))
 
 #kiem tra so nguyen to
 import math
